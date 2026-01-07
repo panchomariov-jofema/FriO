0f6d05a13dbd0c101c7613b9080075a94bdce8af
Francisco Villarreal <panchomariov@gmail.com>
2026-01-07 16:41:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-51741883-8843e",
+  "appId": "1:658428459884:web:11a33869b1ec766a1d5149",
+  "apiKey": "AIzaSyAyUkpe6pHjoGjKiEsXta5q2ppWwrWqeu4",
+  "authDomain": "studio-51741883-8843e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "658428459884"
+};
