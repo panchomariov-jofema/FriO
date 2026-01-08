2e9381366115514ffe8e0b6464b97ebe09c832dd
Francisco Villarreal <panchomariov@gmail.com>
2026-01-08 12:50:37 +0000

    sigue ahi, borra cualquiera de los ultimos 2 ya que cumpliran la misma f

--- src/app/(app)/otros-clientes-frutas/page.tsx
+++
@@ -1,5 +0,0 @@
-import { PlaceholderPage } from "@/components/PlaceholderPage";
-
-export default function OtrosClientesFrutasPage() {
-    return <PlaceholderPage title="Otros Clientes Frutas" />;
-}
