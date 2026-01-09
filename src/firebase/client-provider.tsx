573c2bce3463643952f2219473f53f9bcdf8dc25
Francisco Villarreal <panchomariov@gmail.com>
2026-01-09 13:56:26 +0000

    Al cambiar la posición de las notificaciones (toasts) a la esquina super

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -3,6 +3,7 @@
 import React, { useMemo, type ReactNode } from 'react';
 import { FirebaseProvider } from '@/firebase/provider';
 import { initializeFirebase } from '@/firebase';
+import { Toaster } from '@/components/ui/toaster';
 
 interface FirebaseClientProviderProps {
   children: ReactNode;
@@ -21,6 +22,7 @@ export function FirebaseClientProvider({ children }: FirebaseClientProviderProps
       firestore={firebaseServices.firestore}
     >
       {children}
+      <Toaster />
     </FirebaseProvider>
   );
 }
