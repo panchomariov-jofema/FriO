8405a52fc1700a9a09c5ccca949439de252ac8f9
Francisco Villarreal <panchomariov@gmail.com>
2026-01-12 14:41:56 +0000

    Las Camas de la 1 a la 3, deben incrementar sus columnas desde la A a L.

--- src/lib/chambers-config.ts
+++ src/lib/chambers-config.ts
@@ -4,7 +4,8 @@ interface ChambersConfig {
     [key: string]: Chamber;
 }
 
-const columns = ['A', 'B', 'C', 'D', 'E', 'F'];
+const columns1to3 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'];
+const columns4to6 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O'];
 const rows = Array.from({ length: 13 }, (_, i) => i + 1);
 
 export const chambersConfig: ChambersConfig = {
@@ -12,42 +13,42 @@ export const chambersConfig: ChambersConfig = {
         id: 'CAMARA-1',
         name: 'CÁMARA 1',
         capacity: 800,
-        columns,
+        columns: columns1to3,
         rows,
     },
     'CAMARA-2': {
         id: 'CAMARA-2',
         name: 'CÁMARA 2',
         capacity: 800,
-        columns,
+        columns: columns1to3,
         rows,
     },
     'CAMARA-3': {
         id: 'CAMARA-3',
         name: 'CÁMARA 3',
         capacity: 800,
-        columns,
+        columns: columns1to3,
         rows,
     },
     'CAMARA-4': {
         id: 'CAMARA-4',
         name: 'CÁMARA 4',
         capacity: 1500,
-        columns,
+        columns: columns4to6,
         rows,
     },
     'CAMARA-5': {
         id: 'CAMARA-5',
         name: 'CÁMARA 5',
         capacity: 1500,
-        columns,
+        columns: columns4to6,
         rows,
     },
     'CAMARA-6': {
         id: 'CAMARA-6',
         name: 'CÁMARA 6',
         capacity: 1500,
-        columns,
+        columns: columns4to6,
         rows,
     },
 };
