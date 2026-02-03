dd77763ebf663290404a442dafd0e8bf1452b4de
Francisco Villarreal <panchomariov@gmail.com>
2026-02-03 12:55:39 +0000

    ahora desbloquea todas las ubicaciones de las camaras

--- src/lib/chambers-config.ts
+++ src/lib/chambers-config.ts
@@ -18,31 +18,6 @@ const columns4to6 = [
 
 const rows = Array.from({ length: 14 }, (_, i) => i + 1);
 
-const blockedCoordsC1C3 = [
-    ...Array.from({ length: 5 }, (_, i) => `E${i + 9}`), // E9-E13
-    ...Array.from({ length: 5 }, (_, i) => `F${i + 9}`), // F9-F13
-    ...Array.from({ length: 5 }, (_, i) => `G${i + 9}`), // G9-G13
-    ...Array.from({ length: 5 }, (_, i) => `H${i + 9}`), // H9-H13
-];
-
-const blockedCoordsC4C5 = [
-    ...Array.from({ length: 5 }, (_, i) => `F${i + 9}`), // F9-F13
-    ...Array.from({ length: 5 }, (_, i) => `G${i + 9}`), // G9-G13
-    ...Array.from({ length: 5 }, (_, i) => `H${i + 9}`), // H9-H13
-    ...Array.from({ length: 5 }, (_, i) => `I${i + 9}`), // I9-I13
-    ...Array.from({ length: 5 }, (_, i) => `J${i + 9}`), // J9-J13
-    ...Array.from({ length: 5 }, (_, i) => `K${i + 9}`), // K9-K13
-];
-
-const blockedCoordsC6 = [
-    ...Array.from({ length: 5 }, (_, i) => `K${i + 9}`), // K9-K13
-    ...Array.from({ length: 5 }, (_, i) => `L${i + 9}`), // L9-L13
-    ...Array.from({ length: 5 }, (_, i) => `M${i + 9}`), // M9-M13
-    ...Array.from({ length: 5 }, (_, i) => `N${i + 9}`), // N9-N13
-    ...Array.from({ length: 5 }, (_, i) => `O${i + 9}`), // O9-O13
-];
-
-
 export const chambersConfig: ChambersConfig = {
     'CAMARA-1': {
         id: 'CAMARA-1',
@@ -50,7 +25,6 @@ export const chambersConfig: ChambersConfig = {
         capacity: 800,
         columns: columns1to3,
         rows,
-        blocked: blockedCoordsC1C3,
     },
     'CAMARA-2': {
         id: 'CAMARA-2',
@@ -58,7 +32,6 @@ export const chambersConfig: ChambersConfig = {
         capacity: 800,
         columns: columns1to3,
         rows,
-        blocked: blockedCoordsC1C3,
     },
     'CAMARA-3': {
         id: 'CAMARA-3',
@@ -66,7 +39,6 @@ export const chambersConfig: ChambersConfig = {
         capacity: 800,
         columns: columns1to3,
         rows,
-        blocked: blockedCoordsC1C3,
     },
     'CAMARA-4': {
         id: 'CAMARA-4',
@@ -74,7 +46,6 @@ export const chambersConfig: ChambersConfig = {
         capacity: 1500,
         columns: columns4to6,
         rows,
-        blocked: blockedCoordsC4C5,
     },
     'CAMARA-5': {
         id: 'CAMARA-5',
@@ -82,7 +53,6 @@ export const chambersConfig: ChambersConfig = {
         capacity: 1500,
         columns: columns4to6,
         rows,
-        blocked: blockedCoordsC4C5,
     },
     'CAMARA-6': {
         id: 'CAMARA-6',
@@ -90,7 +60,6 @@ export const chambersConfig: ChambersConfig = {
         capacity: 1500,
         columns: columns4to6,
         rows,
-        blocked: blockedCoordsC6,
     },
 };
 
