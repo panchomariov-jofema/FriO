2d6808bf763da1440806e24328a4623b01c3de17
Francisco Villarreal <panchomariov@gmail.com>
2026-02-05 16:19:47 +0000

    aumenta la virgulilla de la manza y separala para que visualmente se dif

--- src/components/ui/CustomAppleIcon.tsx
+++ src/components/ui/CustomAppleIcon.tsx
@@ -16,7 +16,7 @@ export const CustomAppleIcon = React.forwardRef<SVGSVGElement, React.SVGProps<SV
         {...props}
     >
         <path d="M12 20.94c1.5 0 2.75 1.06 4 1.06 3 0 6-8 6-12.22A4.91 4.91 0 0 0 17 5c-2.22 0-4 1.44-5 2-1-.56-2.78-2-5-2a4.9 4.9 0 0 0-5 4.78C2 14 5 22 8 22c1.25 0 2.5-1.06 4-1.06Z" />
-        <path d="M 9.5 4.5 C 10.5 2.5, 12.5 6.5, 14.5 4.5" />
+        <path d="M15.5 2.5c-1.5-3-5.5-2.5-7 0" />
     </svg>
 ));
 CustomAppleIcon.displayName = 'CustomAppleIcon';
