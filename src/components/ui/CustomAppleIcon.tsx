e1ebe8a9babe51dcf7d5de9bf90109d8d605b57b
Francisco Villarreal <panchomariov@gmail.com>
2026-02-05 16:34:06 +0000

    ok, separala un poco mas de la manzana. No se aprecian bien

--- src/components/ui/CustomAppleIcon.tsx
+++ src/components/ui/CustomAppleIcon.tsx
@@ -16,7 +16,7 @@ export const CustomAppleIcon = React.forwardRef<SVGSVGElement, React.SVGProps<SV
         {...props}
     >
         <path d="M12 20.94c1.5 0 2.75 1.06 4 1.06 3 0 6-8 6-12.22A4.91 4.91 0 0 0 17 5c-2.22 0-4 1.44-5 2-1-.56-2.78-2-5-2a4.9 4.9 0 0 0-5 4.78C2 14 5 22 8 22c1.25 0 2.5-1.06 4-1.06Z" />
-        <path d="M8.5 2.5 C 10 0.5, 13 4.5, 14.5 2.5" stroke="currentColor" strokeWidth="2.5"/>
+        <path d="M8.5 1.0 C 10 -1.0, 13 3.0, 14.5 1.0" stroke="currentColor" strokeWidth="2.5"/>
     </svg>
 ));
 CustomAppleIcon.displayName = 'CustomAppleIcon';
