12f1c7c229db5a34582f78ff2cf03f2b26fc1329
Francisco Villarreal <panchomariov@gmail.com>
2026-02-05 16:30:54 +0000

    ahora ponla de color negro y un poco mas fina

--- src/components/ui/CustomAppleIcon.tsx
+++ src/components/ui/CustomAppleIcon.tsx
@@ -16,7 +16,7 @@ export const CustomAppleIcon = React.forwardRef<SVGSVGElement, React.SVGProps<SV
         {...props}
     >
         <path d="M12 20.94c1.5 0 2.75 1.06 4 1.06 3 0 6-8 6-12.22A4.91 4.91 0 0 0 17 5c-2.22 0-4 1.44-5 2-1-.56-2.78-2-5-2a4.9 4.9 0 0 0-5 4.78C2 14 5 22 8 22c1.25 0 2.5-1.06 4-1.06Z" />
-        <path d="M8.5 1.5 C 9.5 -0.5, 12.5 3.5, 14.5 1.5" />
+        <path d="M8.5 1.5 C 8.2, 4, 12, -0.5, 14.5, 1.5" stroke="hsl(var(--foreground))" strokeWidth="2"/>
     </svg>
 ));
 CustomAppleIcon.displayName = 'CustomAppleIcon';
