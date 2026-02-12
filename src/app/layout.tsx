ac87b221671e2a014a1a5fb7b8b630c56ca2789c
Francisco Villarreal <panchomariov@gmail.com>
2026-02-12 18:16:16 +0000

    se instalo el PWA pero el incono del programa no es el logo del proyecto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/logo.svg" />
         <meta name="theme-color" content="#22c55e" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
